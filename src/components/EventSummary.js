96a7d360127f5ebe63ac62a0e17ba4363da3a9b0
Austin Malmberg <austin.malmberg@gmail.com>
2019-12-06 18:17:54 -0500

    More styling

--- src/components/EventSummary.js
+++ src/components/EventSummary.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const EventSummary = ({ summary }) => {
 
-  const maxLength = 32;
-  if (summary.length > maxLength)
-    summary = `${summary.substring(0, maxLength)}...`;
-
   return (
-    <h3 className="event--summary">{ summary }</h3>
+    <p className="event--summary">{ summary }</p>
   );
 };
 
