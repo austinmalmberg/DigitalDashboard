3d094d71ef5b541f7e6836a84ef8866b4f0a8585
Austin Malmberg <austin.malmberg@gmail.com>
2019-12-10 11:16:27 -0500

    Synchronized tick rate with system clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from 'react';
 
 import { formatTime, isSameDate } from '../helpers/dateTime';
 
-import config from '../config';
-
 const Clock = ({ date, setDate }) => {
 
   const [ dateTime, setDateTime ] = useState(new Date());
 
   // initialize interval to update clock time
   useEffect(() => {
-    const timerId = setInterval(() => setDateTime(new Date()), 1000);
+    let timerId;
+
+    const d = new Date();
+    setDateTime(d);
+
+    // syncs the interval with the system clock so they "tick" together
+    const timeSyncId = setTimeout(() => {
+      timerId = setInterval(() => setDateTime(new Date()), 1000);
+    }, 1000 - d.getTime() % 1000);
 
     return () => {
+      if (timeSyncId) clearTimeout(timeSyncId);
       if (timerId) clearInterval(timerId);
     };
   }, []);
 
-  // called every second
-  // determines if the date changed (not time)
-  // if the date changed, setDate on app.
+  // called every rerender (every second) to determine if the date changed
+  // which updates the calendar in the main app
   useEffect(() => {
     if (!isSameDate(date, dateTime) || !date) {
       setDate(dateTime);
