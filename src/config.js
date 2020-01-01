eeac5443da003f17e27055f126202cdc23499a36
Austin Malmberg <austin.malmberg@gmail.com>
2019-12-31 19:43:06 -0500

    Provided instructions for adding contributors

--- src/config.js
+++ src/config.js
@@ -5,23 +5,18 @@ const config = {
   calendar: {
     calendarId: '5u6lidgv1kp59e0i007hmksd94@group.calendar.google.com', // Default: primary
     daysToSync: 7,  // days. Default: 7
+
+    // used for custom assignee colors
     contributors: [
+      /* FOR DEMONSTRATION PURPOSES ONLY; DO NOT MODIFY
       {
-        email: 'austin.malmberg@gmail.com',
+        email: 'test@email.com',
         displayName: 'Austin',
         style: {
           color: 'white',
           backgroundColor: '#FF524B' // reddish
         }
-      },
-      {
-        email: 'ams91592@gmail.com',
-        displayName: 'Annie',
-        style: {
-          color: 'white',
-          backgroundColor: '#2ED95B' // green
-        }
-      }
+      },*/
     ]
   }
 };
