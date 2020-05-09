f97274292c2a5bd5873729af32595189c7baa0b9
Austin Malmberg <austin.malmberg@gmail.com>
2020-05-09 16:10:46 -0400

    Added additional color

--- src/helpers/skycons.js
+++ src/helpers/skycons.js
@@ -3,30 +3,61 @@ const Skycons = window.Skycons;
 let skycons = new Skycons({ "color": "#dfe6e9"});
 
 const skycon = {
-  "clear-day": Skycons.CLEAR_DAY,
-  "clear-night": Skycons.CLEAR_NIGHT,
-  "partly-cloudy-day": Skycons.PARTLY_CLOUDY_DAY,
-  "partly-cloudy-night": Skycons.PARTLY_CLOUDY_NIGHT,
-  "cloudy": Skycons.CLOUDY,
-  "rain": Skycons.RAIN,
-  "sleet": Skycons.SLEET,
-  "snow": Skycons.SNOW,
-  "wind": Skycons.WIND,
-  "fog": Skycons.FOG
+    "clear-day": {
+        icon: Skycons.CLEAR_DAY,
+        color: "#ffa502",
+    },
+    "clear-night": {
+        icon: Skycons.CLEAR_NIGHT,
+        color: "#ffa502",
+    },
+    "partly-cloudy-day": {
+        icon: Skycons.PARTLY_CLOUDY_DAY,
+        color: "#57606f",
+    },
+    "partly-cloudy-night": {
+        icon: Skycons.PARTLY_CLOUDY_NIGHT,
+        color: "#2f3542",
+    },
+    "cloudy": {
+        icon: Skycons.CLOUDY,
+        color: "#57606f",
+    },
+    "rain": {
+        icon: Skycons.RAIN,
+        color: "#2f3542",
+    },
+    "sleet": {
+        icon: Skycons.SLEET,
+        color: "#57606f",
+    },
+    "snow": {
+        icon: Skycons.SNOW,
+        color: "#ffa502",
+    },
+    "wind": {
+        icon: Skycons.WIND,
+        color: "#ffa502",
+    },
+    "fog": {
+        icon: Skycons.FOG,
+        color: "#ffa502",
+    },
 }
-
-// default
-const def = Skycons.WIND;
 
 function setColor(color) {
-  if (skycons.color !== color) {
-    skycons = new Skycons({ "color": color });
-  }
+    if (skycons.color !== color) {
+        skycons = new Skycons({ "color": color });
+    }
 }
 
-function addSkyconToCanvas(element, description) {
-  skycons.set(element, skycon[description] || def);
-  skycons.play();
+function addSkyconToCanvas(canvas, description) {
+    const { icon, color } = skycon[description] || skycon['wind'];
+
+    setColor(color);
+
+    skycons.set(canvas, icon);
+    skycons.play();
 }
 
-export { addSkyconToCanvas, setColor };
+export default addSkyconToCanvas;
